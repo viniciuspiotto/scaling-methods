b766a864fa401a3cbad4cca2f812eb026c1d34c0
piotto <pgviniciuspiotto@gmail.com>
2023-12-12 20:20:10 -0300

    Implementing interface file/header

---
+++ include/interface.h
@@ -0,0 +1,8 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+void failedChoice();
+int choiceMethod();
+void cleanTerminal();
+
+#endif
---
+++ src/interface.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void failedChoice(){
+printf("*--------------------------------------*\n");
+printf("|            Opção inválida            |\n");
+printf("*--------------------------------------*\n\n\n");
+}
+
+int choiceMethod(){
+    int choice;
+    printf("Escolha um método: \n\n");
+    printf("1. First Come First Serve\n");
+    printf("2. Shortest Job First\n");
+    printf("3. Round Robin\n");
+    printf("\nDigite sua escolha: ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+void cleanTerminal(){
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
