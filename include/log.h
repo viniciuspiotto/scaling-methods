c0a6a747b1ecb9f91f31b2f27fc0677b6558367e
0xbhsu <0xbhsu@mail.net>
2023-12-15 03:18:10 -0300

    Fixing functions prototypes from header file

--- include/log.h
+++ include/log.h
@@ -1,12 +1,13 @@
 #ifndef LOG_H_
 #define LOG_H_
-
 #include <stdio.h>
 #include "process.h"
 
 void logMessage(FILE *file, char *message);
+void logMessageFileName(FILE *file, char *fileName);
 void logCreateProcess(FILE *file, process* currentProcess, int size);
 void logExecuteProcess(FILE *file, process* currentProcess);
 void logFinishProcess(FILE *file, process* currentProcess);
+void logChangeProcess(FILE *file, process* newProcess, process* oldProcess);
 
 #endif
