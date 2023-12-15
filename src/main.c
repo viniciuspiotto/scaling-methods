5b7edaf4c991f77c72bce700c69dcd68f5cb71d1
piotto <pgviniciuspiotto@gmail.com>
2023-12-15 02:45:07 -0300

    Implementing main file

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "interface.h"
+#include "fcfs.h"
+#include "sjf.h"
+
+int main(){
+    srand(time(0));
+    cleanTerminal();
+    char fileName[50];
+    sprintf(fileName, "relatorio-%d.txt", (int)time(NULL));
+    FILE* file = fopen(fileName, "w");
+    if(file == NULL){
+        printf("Erro ao criar arquivo de relatório.\n");
+        exit(1);
+    }
+
+    int choice = 0;
+    do {
+        choice = choiceMethod();
+        switch(choice){
+            case 1:
+                cleanTerminal();
+                logMessageFileName(file, fileName);
+                firstComeFirstServed(file);
+                break;
+            case 2:
+                cleanTerminal();
+                logMessageFileName(file, fileName);
+                shortestJobFirst(file);
+                break;
+            case 3:
+                cleanTerminal();
+                logMessageFileName(file, fileName);
+                roundRobin(file);
+                break;
+            default:
+                cleanTerminal();
+                failedChoice();
+                break;
+        }
+    } while(choice > 3 || choice < 1);
+    fclose(file);
+}
